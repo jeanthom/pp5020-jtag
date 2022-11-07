6e34cf2cde1b6811239775e15d4c1b425da1c077
Jean THOMAS <virgule@jeanthomas.me>
2022-11-07 22:04:26 +0100

    Fix CPU/COP position

--- openocd.tcl
+++ openocd.tcl
@@ -5,6 +5,6 @@ transport select jtag
 jtag newtap pp5002 cpu -irlen 4 -expected-id 0x2f1f0f0f
 jtag newtap pp5002 cop -irlen 4 -expected-id 0x2f1f0f0f
 
-target create pp5002.cpu arm7tdmi -chain-position pp5002.cpu
 target create pp5002.cop arm7tdmi -chain-position pp5002.cop
+target create pp5002.cpu arm7tdmi -chain-position pp5002.cpu
 
